34698ce301f0386ff19a1fbfb8312073253ffd28
SockLessBot <civilisation2.0@gmail.com>
2025-02-02 10:50:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         let user = window.Telegram.WebApp.initDataUnsafe.user;
         if (user) {
+            // Initialisation du message de bienvenue avec le nom de l'utilisateur
             document.getElementById('app').textContent = `Bienvenue, ${user.first_name}!`;
         }
-    } else {
-        alert("Cette page est conçue pour être ouverte via Telegram.");
-    }
-});
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (window.Telegram.WebApp) {
-        window.Telegram.WebApp.ready();
-        window.Telegram.WebApp.expand();
 
-        // Ajoute ici la logique de ton jeu ou interaction
+        // Ajout du bouton "Jouer"
         let welcomeMessage = document.getElementById('app');
         let playButton = document.createElement('button');
         playButton.textContent = 'Jouer';
         playButton.onclick = function() {
             // Logique du jeu ici
             alert('Tu as cliqué sur Jouer!');
-            // Par exemple, tu pourrais changer le texte de bienvenue
+            // Changement du message de bienvenue pour indiquer que le jeu commence
             welcomeMessage.textContent = 'Le jeu commence!';
         };
         document.body.appendChild(playButton);
